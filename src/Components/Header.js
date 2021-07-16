497fde2bec3e03c85a0aeee3d8fa097d39b445a0
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-07-16 17:16:06 -0300

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import { ReactComponent as Dogs } from '../Assets/dogs.svg'
 
 const Header = () => {
   return (
-    <div className={styles.header}> {/* estilo específico */}
-      <nav className="container"> {/* estilo global */}
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}> {/* estilo específico, dinâmico */}
+      <nav className={`${styles.nav} container`}> {/* estilo global, estático (container) */}
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home"> {/* aria-label é como o alt da tag <img>, ajuda na acessibilidade */}
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">Login / Criar</Link>
       </nav>
-    </div>
+    </header>
   )
 }
 
