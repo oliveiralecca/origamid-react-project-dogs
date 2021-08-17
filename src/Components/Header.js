f3ef8bcfb3fab8d5f157693d1cbaf15c0e28c0b1
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-08-16 21:33:32 -0300

    context created

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,15 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 import { ReactComponent as Dogs } from '../Assets/dogs.svg'
+import { UserContext } from '../UserContext'
 
 const Header = () => {
+  const { data } = React.useContext(UserContext)
+
   return (
     <header className={styles.header}> {/* estilo específico, dinâmico */}
       <nav className={`${styles.nav} container`}> {/* estilo global, estático (container) */}
         <Link className={styles.logo} to="/" aria-label="Dogs - Home"> {/* aria-label é como o alt da tag <img>, ajuda na acessibilidade */}
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link> 
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}               
       </nav>
     </header>
   )
---
+++ src/UserContext.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { TOKEN_POST, USER_GET } from './api'
+
+export const UserContext = React.createContext()
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = React.useState(null)
+  const [login, setLogin] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token)
+    const response = await fetch(url, options)
+    const json = await response.json()
+    setData(json)
+    setLogin(true)
+  }
+
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password })
+    const tokenResponse = await fetch(url, options)
+    const { token } = await tokenResponse.json()
+    window.localStorage.setItem('token', token)
+    getUser(token)
+  } // se o token do usuário já estiver no localStorage, puxa de lá ao invés de fazer o fetch para pegar o token novamente
+
+  return (
+    <UserContext.Provider value={{ userLogin, data }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
